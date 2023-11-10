68be8ebca95c43ddd0ea94e224c26d7e0c9d575d
Nicolas <70273074+Nicolascoitras@users.noreply.github.com>
2023-11-10 12:09:32 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,54 @@
+# # # # # lista = [1,2,3]
+# # # # print("\U0001F600")
+
+# # # # print('Atividade 1 :)')
+# # # # lista = []
+# # # # while True:
+# # # #     nomeDoProduto = input('Digite o nome do produto: ') 
+# # # #     valorDoProduto = int(input('Digite o preço do produto: '))
+# # # #     lista.append(valorDoProduto)
+# # # #     continuar = input('Deseja adicionar mais produtos? S/N').lower()
+    
+# # # #     if continuar == "n":
+# # # #         print(f"Preço a pagar: {sum(lista)} ")
+# # # #         break
+
+# # # # while True:
+# # # #  senha = input('Digite sua senha, a senha deve ter no minimo 8 digitos e no maximo 12 digitos: ')
+# # # #  if len(senha) < 8:
+# # # #     print("Senha muito pequena")
+# # # #  elif len(senha) > 12:
+# # # #   print('Senha muito grande')
+# # # #  if len(senha) >= 8 and len(senha ) <= 12:
+# # # #   print("Senha aprovada")
+# # # #   break
+
+# # # while True:
+# # #     palavra = input('digite qualquer palavra: ')
+# # #     if palavra == palavra.upper():
+# # #         print(palavra,"Maiusculo")
+# # #     else: print(palavra,"Minusculo")
+# # #     question = input("deseja continuar verficando? [Y/N]")
+# # #     if question == "n":
+# # #         break
+
+# # for numero in range(10):
+# #     if numero == 5:
+# #         break
+# #     print(numero)
+    
+# # numero = 0
+# # while numero < 10:
+# #     if numero == 5:
+# #         break
+# #     print(numero)
+# #     numero += 1
+
+# cont = 0 
+# while cont <= 5:
+#     if cont == 4:
+#         continue
+#     print(f"Numero {cont}")
+#     cont += 1
+#     print('fim do teste')
+        
